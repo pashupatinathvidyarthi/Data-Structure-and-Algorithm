cc2209a8ed5b5eeec45bdd2a93020afec659809e
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-02-18 23:44:56 +0530

    Cpp File

--- lec01.cpp
+++ lec01.cpp
@@ -1,7 +1,24 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main()
 {
     cout<<"Hello World!"<<endl;
+    return 0;
+}*/
+
+
+#include<bits/stdc++.h> 
+/* This header file includes all the standard C++ libraries. 
+It is a convenient way to include all the necessary libraries in one line, 
+but it may increase compilation time. 
+It is generally recommended to include only the specific libraries 
+that are needed for better performance.
+*/
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    cout<<"Value of x is:"<< x << "and value of y is:"<< y <<endl;
     return 0;
 }
