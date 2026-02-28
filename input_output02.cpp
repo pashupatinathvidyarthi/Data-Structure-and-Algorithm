2a04e1bd452ba60c9e50232fff9197f241fa24d2
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-02-28 19:10:22 +0530

    DSA

--- input_output02.cpp
+++ input_output02.cpp
@@ -1,7 +1,15 @@
+// #include<iostream>
+// int main()
+// {
+//     std::cout<<"Hey Pashupati"<<'\n';
+//     std::cout<<"Hey Pashupati";
+//     return 0;
+// }
+
 #include<iostream>
 int main()
 {
-    std::cout<<"Hey Pashupati"<<'\n';
-    std::cout<<"Hey Pashupati";
+    std::cout<<"Pashupati Nath"<<std::endl;
+    std::cout<<"Pashupati Nath";
     return 0;
 }
