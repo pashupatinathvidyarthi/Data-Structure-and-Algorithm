2a5bd64f54276d9dcb54a1a462c76d835a0adb93
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-02-28 19:31:59 +0530

    DSA

--- variables.cpp
+++ variables.cpp
@@ -1,9 +1,21 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cin>>x;
+//     cout<<"You have entered : "<<x;
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-    int x;
-    cin>>x;
-    cout<<"You have entered : "<<x;
+    int x,y;
+    cin >> x >> y;
+    cout<<"You have entered the value of x is "<<x<<" and value of y is "<<y;
     return 0;
 }
