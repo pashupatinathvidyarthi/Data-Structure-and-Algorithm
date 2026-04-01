5830e6285c4cd6f90683c218094ffbde67ab5922
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-01 15:37:24 +0530

    DSA

--- if_else_statements.cpp
+++ if_else_statements.cpp
@@ -1,7 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+//Write a program that takes an input of age
+//and prints if you are adult or not
 int main()
 {
-    
+    int age;
+    cin >> age;
+    if(age>=18)
+        cout<<"You are Adult.";
+    else
+        cout<<"You are not Adult!";
     return 0;
 }
