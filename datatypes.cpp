e26ed7120a9112125fcf213e11c03ac357e0f87f
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-01 14:50:43 +0530

    DSA

--- datatypes.cpp
+++ datatypes.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    //int
+    int x=10;
+    //long
+    long x=15;
+    cin>>x;
+
+    long long x=1500000;
+
+
+    //float, double
+    float x=5.6;
+    float y=5;
+    // integer can also store in double
+    double z=5.674;
     return 0;
 }
