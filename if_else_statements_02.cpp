e5e89d70045d111a873563120ef76df56ca3fe93
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-01 15:43:23 +0530

    DSA

---
+++ if_else_statements_02.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+A school has following rules for grading system.
+a. Below 25 - F
+b. 25 to 44 - E
+c. 45 to 49 - D
+d. 50 to 59 - C
+e. 60 to 79 - B
+f. 80 to 100 - A
+Ask user to enter marks and print the corresponding grade.
+*/
+int main()
+{
+    return 0;
+}
