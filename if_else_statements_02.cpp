ef49179b32fa8bdd9c699b5c35455a2f1b640f10
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-01 16:00:21 +0530

    DSA

--- if_else_statements_02.cpp
+++ if_else_statements_02.cpp
@@ -10,21 +10,51 @@ e. 60 to 79 - B
 f. 80 to 100 - A
 Ask user to enter marks and print the corresponding grade.
 */
+
+
+
+//Method 01
+
+// int main()
+// {
+//     float marks;
+//     cin >> marks;
+//     if(marks<25)
+//         cout<<"You grade is \"F\"";
+//     else if(marks>=25 && marks <=44)
+//         cout<<"Your grade is \"E\"";
+//         else if(marks>44 && marks<=49)
+//             cout<<"Your grade is \"D\"";
+//             else if(marks>49 && marks <=59)
+//                 cout<<"Your grade is \"C\"";
+//                 else if(marks>59 && marks <=79)
+//                     cout<<"Your grade is \"B\"";
+//                     else if(marks>79 && marks <=100)
+//                         cout<<"Your grade is \"A\"";
+//     return 0;
+// }
+
+
+
+
+//Method 02
 int main()
 {
     float marks;
     cin >> marks;
     if(marks<25)
         cout<<"You grade is \"F\"";
-    else if(marks>25 && marks <=44)
+    else if(marks <=44)
         cout<<"Your grade is \"E\"";
-        else if(marks>44 && marks<=49)
+        else if(marks<=49)
             cout<<"Your grade is \"D\"";
-            else if(marks>49 && marks <=59)
+            else if(marks <=59)
                 cout<<"Your grade is \"C\"";
-                else if(marks>59 && marks <=79)
+                else if(marks <=79)
                     cout<<"Your grade is \"B\"";
-                    else if(marks>79 && marks <=100)
+                    else if(marks <=100)
                         cout<<"Your grade is \"A\"";
     return 0;
 }
+
+
